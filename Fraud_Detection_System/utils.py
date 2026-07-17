332b522c4cc2c5bfb2b2f9cea4506b0b49ba47a6
Your Moreshwar Pachbhai <moreshwarpachbhai490@gmail.com>
2026-07-17 23:57:02 +0530

    Add missing project files

--- Fraud_Detection_System/utils.py
+++ Fraud_Detection_System/utils.py
@@ -1,7 +1,13 @@
 from pathlib import Path
+import streamlit as st
 import pandas as pd
 
 BASE_DIR = Path(__file__).resolve().parent
 
+st.write("BASE_DIR =", BASE_DIR)
+st.write("Files =", list(BASE_DIR.iterdir()))
+
 def load_data():
-    return pd.read_csv(BASE_DIR / "fraud_data.csv")
+    csv_path = BASE_DIR / "fraud_data.csv"
+    st.write("CSV Path =", csv_path)
+    return pd.read_csv(csv_path)
